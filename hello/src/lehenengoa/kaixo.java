118a1f0df1a481ff9b9919c19a78a9253e3928fd
ieltzu <ieltzu@ieltzu-Lenovo-IdeaPad-Z500>
2014-10-03 16:58:19 +0200

    lehenengo bertsioa

---
+++ hello/src/lehenengoa/kaixo.java
@@ -0,0 +1,15 @@
+package lehenengoa;
+
+public class kaixo {
+	private String mezua;
+	public kaixo() {
+		mezua="Kaixo Mundua!";
+	}
+	public String getMezua() {
+		return mezua;
+	}
+	public void inprimatu(){
+		System.out.println(this.getMezua());
+	}
+}
+
